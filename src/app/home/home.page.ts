138ae752cd5b292f7f442900db5ea0722937ade8
Esinniobiwa Quareeb <esinniobiwa.quareeb@gmail.com>
2019-05-14 11:31:32 +0100

    Toast Controller Added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  quotes: Observable<any>;
+  constructor(private http: HttpClient, public toastController: ToastController) {
+    this.showQuote();
+  }
 
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Loading...',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  showQuote() {
+    this.presentToast();
+    this.quotes = this.http.get('https://quota.glitch.me/random');
+    this.quotes.subscribe(data => {
+      this.quotes = data;
+      this.toastController.dismiss();
+      console.log(this.quotes);
+    });
+  }
 }
